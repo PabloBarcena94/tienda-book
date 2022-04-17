b30f0e915c34ca0dda5b8a9b735d8c35ae5b0f4e
unknown <pabloandresbarcena@gmail.com>
2022-04-17 16:03:56 -0300

    Agregué un NavBar y un props

---
+++ src/components/CartWidget/CartWidget.js
@@ -0,0 +1,10 @@
+import cart from './Img/cart.png'
+import './CartWidget.css'
+
+
+export function CartWidget () {
+        return (
+
+                <img className="carrito" src = {cart} />
+        );
+}
---
+++ src/components/ListContainer/ItemListContainer.js
@@ -0,0 +1,8 @@
+export function ItemListContainer (props) {
+    return (
+            <div>
+              <h2>{props.userName}</h2>
+            </div>
+    );
+  }
+
